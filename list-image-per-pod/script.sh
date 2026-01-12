6a24e97f1234ce4ec0f5095b2150387f802ffe23
carlozrm90 <carloszrm90@gmail.com>
2026-01-12 15:24:55 -0600

    add file output  in function list image per pod

--- list-image-per-pod/script.sh
+++ list-image-per-pod/script.sh
@@ -2,12 +2,13 @@
 
 # Loop through every namespace
 for ns in $(oc get namespaces -o jsonpath='{.items[*].metadata.name}'); do
-    echo "=========================================================="
+    echo "==========================================================" >> output.log
     echo " NAMESPACE: $ns"
+    echo " NAMESPACE: $ns" >>  output.log
     echo "=========================================================="
     
     # Run your specific formatter
-    oc get pods -n "$ns" -o jsonpath='{range .items[*]}{"\n"}POD: {.metadata.name}{range .spec.containers[*]}{"\n"}  - CONTAINER: {.name}{"\n"}    IMAGE:     {.image}{end}{"\n"}{end}'
+    oc get pods -n "$ns" -o jsonpath='{range .items[*]}{"\n"}POD: {.metadata.name}{range .spec.containers[*]}{"\n"}  - CONTAINER: {.name}{"\n"}    IMAGE:     {.image}{end}{"\n"}{end}' >>  output.log
     
-    echo -e "\n" # Adds extra spacing between namespaces
+    echo -e "\n" >>  output.log # Adds extra spacing between namespaces 
 done
